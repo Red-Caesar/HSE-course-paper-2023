6059b15f7d98eef7666081929a3d73359cf778e2
Red-Caesar <suslovavd2002@mail.ru>
2023-05-24 10:41:46 +0300

    add to chose custome numbers of city

--- parseTSP.py
+++ parseTSP.py
@@ -1,14 +1,21 @@
 import xml.etree.ElementTree as ET
 from typing import Tuple, List
 
-def parsing(filename: str, fill_same: float) -> List[List[float]]:
+def parsing(filename: str, fill_same: float, city_numbers: int=None) -> List[List[float]]:
     tree = ET.parse(f'data/{filename}')
     root = tree.getroot()
     graph = root.find('graph')
     matrix = []
-    n = len(graph.findall('vertex'))
+    if not city_numbers:
+        n = len(graph.findall('vertex'))
+    else:
+        n = city_numbers
     for i, ver in enumerate(graph.findall('vertex')):
+        if i >= n:
+            continue
         matrix.append([fill_same for _ in range(n)])
         for edge in ver.findall('edge'):
+            if int(edge.text) >= n:
+                continue
             matrix[i][int(edge.text)] = float(edge.get('cost'))
     return matrix
