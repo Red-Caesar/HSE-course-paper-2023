09e3735bec60b882e8b8d6ffe56a3a39ecea1faf
Red-Caesar <suslovavd2002@mail.ru>
2023-06-06 10:31:03 +0300

    fix bugs

--- BnB2.py
+++ BnB2.py
@@ -82,7 +82,7 @@ class BnBMethod:
             node.lower_bound = node.prev_cost + self.find_lower_bound(node.excluded_matrix, node.included[:])
             node.upper_bound, path = self.find_upper_bound(node.excluded_matrix, node.included[:], node.prev_cost)
 
-            if node.lower_bound > record.cost:
+            if node.lower_bound >= record.cost or node.upper_bound >= float('inf'):
                 continue
             if node.upper_bound < record.cost:
                 record.cost, record.path = node.upper_bound, path
@@ -91,11 +91,11 @@ class BnBMethod:
                 if next_branch not in node.included:
                     excluded_matrix = np.copy(node.excluded_matrix)
                     excluded_matrix[node.included[-1]][next_branch] = float('inf')
-                    children_exclude = self.Node(0, 0, level, node.included, excluded_matrix)
+                    children_exclude = self.Node(0, 0, level - 1, node.included, excluded_matrix)
                     children_exclude.prev_cost = node.prev_cost
                     queue.append(children_exclude)
                     children_include = self.Node(0, 0, level, node.included + [next_branch], node.excluded_matrix)
-                    children_include.prev_cost = node.prev_cost + matrix[node.included[-1]][next_branch]
+                    children_include.prev_cost = node.prev_cost + node.excluded_matrix[node.included[-1]][next_branch]
                     queue.append(children_include)
                     break
                     
@@ -113,3 +113,8 @@ class BnBMethod:
     
     def getMeanTime(self) -> float:
         return np.mean(self.times)
+
+from parseTSP import parsing
+matrix, _ = parsing('gr24', float('inf'), 6)
+BnB_model = BnBMethod()
+print(BnB_model.start(np.array(matrix), 1), BnB_model.getMeanTime())
